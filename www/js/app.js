f9072799d58bac9fe72085339ec971b6ade45e66
enkhturBaina <eegii.111.gg@gmail.com>
2022-06-23 16:40:41 +0800

    zaswarr

--- www/js/app.js
+++ www/js/app.js
@@ -324,7 +324,7 @@ var app = angular
   })
   .controller("index", function ($scope, $ionicPlatform, $state) {})
   .controller("indexCtrl", function ($scope, $rootScope, $state, $ionicPopup) {
-    $rootScope.zeelmeAppVersion = "2.2.8";
+    $rootScope.zeelmeAppVersion = "2.2.9";
     $scope.toggleSideMenu = function () {
       $("#mobile").toggleClass("non-navigation");
       $("#mobile").toggleClass("navigation");
